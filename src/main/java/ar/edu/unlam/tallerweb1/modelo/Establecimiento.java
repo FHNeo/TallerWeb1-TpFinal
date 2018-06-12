d5955efdeb134129af833bc136cf2217cde0f80f
AguusLu <agus.luu@gmail.com>
2018-06-12 17:38:31 -0300

    Add files via upload
    agrego establecimiento

---
+++ src/main/java/ar/edu/unlam/tallerweb1/modelo/Establecimiento.java
@@ -0,0 +1,60 @@
+package ar.edu.unlam.tallerweb1.modelo;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class Establecimiento {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String nombre;
+	
+	private Float longitud;
+	
+	private Float latitud;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Float getLongitud() {
+		return longitud;
+	}
+
+	public void setLongitud(Float longitud) {
+		this.longitud = longitud;
+	}
+
+	public Float getLatitud() {
+		return latitud;
+	}
+
+	public void setLatitud(Float latitud) {
+		this.latitud = latitud;
+	}
+
+	@Override
+	public String toString() {
+		return "Establecimiento [id=" + id + ", nombre=" + nombre + ", longitud=" + longitud + ", latitud=" + latitud
+				+ "]";
+	}
+	
+	
+	
+	
+}
